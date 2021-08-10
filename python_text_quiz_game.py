6c99616b9ea7d74188950d215ed54bd6fb915901
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-10 19:10:05 +0100

    Add files via upload

--- python_text_quiz_game.py
+++ python_text_quiz_game.py
@@ -2,6 +2,26 @@
 Python game project made with freecodecamp.org and Tech with Tim
 
 '''
+
+
+
+
+def game_name_change():
+    '''Game name changer function'''
+    user_new_name=str(input('Enter your new name, we promise not to judge: '))
+    name_checker =isinstance(user_new_name, str)
+    if name_checker == True:
+        game_age_checker(user_new_name)
+        
+   # elif user_choice_2:
+    #    game_age_checker()
+
+    else:
+        print('Sorry I dont understand')
+        game_name_change()
+
+
+'''
 print ("Welcome to the game!") #print statement
 name = str(input("What is your name? ")) #input statement
 print ('Are you sure', name ,' is your name?') #prints the name in the sentenace
@@ -10,59 +30,92 @@ user_name_confirm=input()
 
 #def confirmation_screen(user_name_confirm):
     
-if user_name_confirm =="yes" or "Yes":
+if user_name_confirm =="yes" or user_name_confirm=="Yes":
     print("Lets continue ",name)
     game_age_checker()
 
-elif user_name_confirm =="No" or "no":
+elif user_name_confirm =="No" or user_name_confirm=="no":
     print("Would you like to change your name? ")
     user_choice_2=input()
-
-    if user_choice_2 =="yes":
-        user_new_name=str(input('Enter your new name, we promise not to judge: '))
-        game_age_checker()
-        
-    elif user_choice_2:
-        game_age_checker()
+    game_name_change()
 
 else:
     print('Sorry I dont understand')
     #welcome_screen()
-
-
-def welcome_screen():
-    print ("Welcome to the game!") #print statement
-    name = str(input("What is your name? ")) #input statement
-    print ('Are you sure', name ,' is your name?') #prints the name in the sentenace
-    user_name_confirm=input()
-    #confirmation_screen(user_name_confirm)
-        
+'''
 
 
 
-def game_age_checker():
-    '''game age checker function'''
-    user_age=str(input('Are you over 18 years old to play the game? '))
-
-    if user_age > 18:
-        print('Even if you lied we arent responible happy gaming')
-        quiz_start()
-
-    else:
-        quiz_end()
-
-
-
-def quiz_start():
-    #function for the quiz game to begin
-   
-
-    
-    print("Before your journey", name ,'can continue select your type class')
-    print('Archer-')
 
 def quiz_end():
     print("This is the end")
 
-#def
-#function for the user to change their name
+def game_age_checker(name):
+    '''game age checker function'''
+    user_age=str(input('Are you over 18 years old to play the game? '))
+    user_age_input_2 = int(input('Please enter your age: '))
+
+    if user_age_input_2 >= 18:
+        print('Even if you lied we arent responible happy gaming')
+        quiz_start(name)
+        
+    elif user_age_input_2 < 18:
+        print('Sorry', name, ' you are too young to play this game')
+            
+
+    else:
+        while user_age_input.isdigit() ==True:
+            try:
+                user_age_input_2 = int(input('Please enter your age: '))
+
+                if user_age_input_2 >= 18:
+                    print('Even if you lied we arent responible happy gaming')
+                    quiz_start(name)
+        
+                elif user_age_input_2 < 18:
+                    print('Sorry', name, ' you are too young to play this game')
+
+                else:
+                    print('You can not play this game')
+            #game_age_checker(name)
+        #quiz_end()
+
+def quiz_start(name):
+    #function for the quiz game to begin
+   
+    print("Before your journey", name ,'can continue select your type class')
+    print('Archer-')
+    
+
+
+
+
+
+
+def welcome_screen():
+    print ("Welcome to the game!") #print statement
+    name = input("What is your name? ") #input statement
+    print ('Are you sure', name ,' is your name? ') #prints the name in the sentenace
+    user_name_confirm=str(input())
+    #confirmation_screen(user_name_confirm)
+        
+    if user_name_confirm =="yes" or user_name_confirm=="Yes":
+        print("Lets continue ",name)
+        game_age_checker(name)
+
+    elif user_name_confirm =="No" or user_name_confirm=="no":
+        print("Would you like to change your name? ")
+        user_choice_2 =str(input())
+        if user_choice_2 == 'yes' or user_choice_2 == 'Yes':
+            game_name_change()
+
+    else:
+        print('Sorry I dont understand')
+        welcome_screen()
+        
+
+
+#runs the first function we have made above
+if __name__=='__main__':
+    welcome_screen().run()
+    
